5e2168e3ffcd0f48bdadc3a5b65056e21203c983
Danveer Gajendra <shineas011@gmail.com>
2023-07-03 17:02:32 +0530

    modification

--- accounts/urls.py
+++ accounts/urls.py
@@ -10,5 +10,9 @@ urlpatterns = [
     path('customerDashboard/', views.customerDashboard, name="customerDashboard"),
     path('restaurantDashboard/', views.restaurantDashboard, name="restaurantDashboard"),
     path('myAccount/', views.myAccount, name="myAccount"),
+    path('activate/<uidb64>/<token>', views.activate, name='activate'),
+    path('forgot_password/', views.forgot_password, name="forgot_password"),
+    path('reset_password_validate/<uidb64>/<token>', views.reset_password_validate, name='reset_password_validate'),
+    path('reset_password/', views.reset_password, name="reset_password"),
 
 ]
--- foodOnline_main/settings.py
+++ foodOnline_main/settings.py
@@ -143,3 +143,12 @@ MESSAGE_TAGS = {
     messages.ERROR: "danger",
   
 }
+
+
+# Email Configuration
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
