94f471e02d0b81a7e966151b0c228ade81a65633
Danveer Gajendra <shineas011@gmail.com>
2023-07-24 14:21:07 +0530

    RazorPay Payment method implementation

--- orders/views.py
+++ orders/views.py
@@ -9,9 +9,11 @@ from orders.models import Order, OrderedFood, Payment
 from .forms import OrderForm
 from .utils import generate_order_number
 import simplejson as json
-
+import razorpay
 from django.contrib.auth.decorators import login_required
+from foodOnline_main.settings import RZP_KEY_ID, RZP_KEY_SECRET
 
+client = razorpay.Client(auth=(RZP_KEY_ID, RZP_KEY_SECRET))
 
 @login_required(login_url='login')
 def place_order(request):
@@ -46,11 +48,27 @@ def place_order(request):
             order.payment_method = request.POST['payment_method']
             order.save()  # order id/ pk is generated
             order.order_number = generate_order_number(order.id)
+            order.save()
+            #RazorPay Payment
+            DATA = {
+                "amount": float(order.total)*100,
+                "currency": "INR",
+                "receipt": "receipt#"+ order.order_number,
+                "notes": {
+                    "key1": "value3",
+                    "key2": "value2"
+                }}
+            rzp_order = client.order.create(data=DATA)
+            rzp_order_id = rzp_order['id']
+           
+            print(rzp_order)
             context = {
                 'order': order,
                 'cart_items': cart_items,
+                'rzp_order_id':  rzp_order_id,
+                'RZP_KEY_ID': RZP_KEY_ID,
+                'rzp_amount': float(order.total)*100,
             }
-            order.save()
             return render(request, 'orders/place_order.html', context)
 
         else:
@@ -150,4 +168,6 @@ def order_complete(request):
         return render(request, 'orders/order_complete.html', context)
     except:
         return redirect()
+    
+
     
